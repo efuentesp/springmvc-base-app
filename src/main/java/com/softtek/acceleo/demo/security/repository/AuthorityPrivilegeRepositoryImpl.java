607c2ddc853b27fb71b6dacce65eece3666d7d98
jaas79 <37114099+jaas79@users.noreply.github.com>
2018-07-31 15:43:29 -0500

    Update AuthorityPrivilegeRepositoryImpl.java
    Se eliminan import que son de test

--- src/main/java/com/softtek/acceleo/demo/security/repository/AuthorityPrivilegeRepositoryImpl.java
+++ src/main/java/com/softtek/acceleo/demo/security/repository/AuthorityPrivilegeRepositoryImpl.java
@@ -11,8 +11,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
